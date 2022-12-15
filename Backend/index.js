b4a9fee6b9f8da5a98effe35e50a954da4b1e78d
Devansh Bansal <devanshd0@gmail.com>
2022-12-14 19:06:11 -0800

    Updating backend files

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
+const axios = require('axios')
 
-app.get('/', (req, res) => {
+
+app.use(cors())
+
+app.get('/test', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/getWalletBalance', (req, result) => {
+  var chainID = req.query.chainID;
+  axios.get('https://api.covalenthq.com/v1/'+chainID+'/address/0x6AE65a7033a84bb36778fEA6607A25a0d6c8EE50/balances_v2/?key=ckey_af34717a92384b14b858f3d0d42', { 
+    headers: { "Accept-Encoding": "gzip,deflate,compress" } 
+})
+  
+  // Show response data
+  .then(res => result.send(res.data))
+  .catch(err => result.send(err))
+  
 })
 
 app.listen(port, () => {
